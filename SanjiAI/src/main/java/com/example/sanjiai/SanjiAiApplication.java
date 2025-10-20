6bb5344701803828a58f4192ca34999903071ae1
Jiin <chloe02.han@gmail.com>
2025-10-21 08:51:40 +0900

    Initial Commit

---
+++ SanjiAI/src/main/java/com/example/sanjiai/SanjiAiApplication.java
@@ -0,0 +1,13 @@
+package com.example.sanjiai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SanjiAiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SanjiAiApplication.class, args);
+    }
+
+}
---
+++ SanjiAI/src/test/java/com/example/sanjiai/SanjiAiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sanjiai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SanjiAiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
